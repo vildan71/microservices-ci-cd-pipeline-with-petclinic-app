b5059b377caf4dfcc1b1bf4e70ee1c950c62a842
vildan71 <arslanv076@gmail.com>
2020-10-11 14:26:53 -0400

    v2

--- build-dev-docker-images.sh
+++ build-dev-docker-images.sh
@@ -3,7 +3,7 @@
 docker build --force-rm -t "petclinic-admin-server:dev" ./spring-petclinic-admin-server
 docker build --force-rm -t "petclinic-api-gateway:dev" ./spring-petclinic-api-gateway
 docker build --force-rm -t "petclinic-config-server:dev" ./spring-petclinic-config-server
-docker build --force-rm -t "petclinic-customer-service:dev" ./spring-petclinic-customer-service
+docker build --force-rm -t "petclinic-customers-service:dev" ./spring-petclinic-customers-service
 docker build --force-rm -t "petclinic-discovery-server:dev" ./spring-petclinic-discovery-server
 docker build --force-rm -t "petclinic-hystrix-dashboard:dev" ./spring-petclinic-hystrix-dashboard
 docker build --force-rm -t "petclinic-vets-service:dev" ./spring-petclinic-vets-service
